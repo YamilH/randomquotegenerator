05a2035295f8952514642efb8308cdfa26c4920a
YamilH <yamilhamui@gmail.com>
2023-04-06 17:01:45 -0300

    upload limpio

---
+++ js/code.js
@@ -0,0 +1,262 @@
+var quotes = [
+	{
+		quote: "La educación no cambia el mundo, cambia a las personas que van a cambiar el mundo.",
+		source: "Paulo Freire",
+		year: "1970"
+	},
+	{
+		quote: "La liberación no es obra exclusiva del líder; es, en primer lugar, obra del oprimido.",
+		source: "Paulo Freire",
+		year: "1970"
+	},
+	{
+		quote: "No hay palabra verdadera que no sea acción, no hay acción verdadera que no sea palabra.",
+		source: "Paulo Freire",
+		year: "1987"
+	},
+	{
+		quote: "La democracia se hace verdadera cuando se construye día a día en la práctica de la libertad.",
+		source: "Paulo Freire",
+		year: "1993"
+	},
+	{
+		quote: "La educación es el arma más poderosa que puedes usar para cambiar el mundo.",
+		source: "Nelson Mandela",
+		year: "1990"
+	},
+	{
+		quote: "La pobreza no es natural, es creada por el hombre y puede superarse y erradicarse mediante acciones de los seres humanos.",
+		source: "Nelson Mandela",
+		year: "2005"
+	},
+	{
+		quote: "No podemos cambiar nada si no lo aceptamos. La condena no libera, oprime.",
+		source: "Carl Jung",
+		year: "1934"
+	},
+	{
+		quote: "Nadie puede hacer que te sientas inferior sin tu consentimiento.",
+		source: "Eleanor Roosevelt",
+		year: "1940"
+	},
+	{
+		quote: "La función de la educación es enseñar a pensar intensa y críticamente. La inteligencia debe trabajarse sin descanso.",
+		source: "Robert Maynard Hutchins",
+		year: "1953"
+	},
+	{
+		quote: "La verdadera libertad es la libertad de pensar de manera diferente.",
+		source: "Rosa Luxemburg",
+		year: "1917"
+		},
+	{
+		quote: "La ignorancia, la indiferencia y la apatía son el mayor enemigo de la justicia.",
+		source: "Rosa Luxemburg",
+		year: "1918"
+		},
+		{
+			quote: "La verdadera patria del hombre es su infancia.",
+			source: "Eduardo Galeano",
+			year: "1978"
+		},
+		{
+			quote: "La educación es el medio más efectivo para cambiar el mundo.",
+			source: "Fernando Savater",
+			year: "2007"
+		},
+		{
+			quote: "La justicia social es la base de la solidaridad y la paz.",
+			source: "Lula da Silva",
+			year: "2004"
+		},
+		{
+			quote: "El único verdadero viaje de descubrimiento consiste no en buscar nuevos paisajes, sino en mirar con nuevos ojos.",
+			source: "Marcel Proust",
+			year: "1913"
+		},
+		{
+			quote: "La lucha por la justicia es la lucha por la dignidad humana.",
+			source: "Luis Alberto Spinetta",
+			year: "1994"
+		},
+		{
+			quote: "Un pueblo sin educación no tiene futuro.",
+			source: "Gabriel García Márquez",
+			year: "1992"
+		},
+		{
+			quote: "El verdadero poder es el poder de transformar las cosas.",
+			source: "Mario Benedetti",
+			year: "1976"
+		},
+		{
+			quote: "La justicia no es un regalo de los cielos, se conquista aquí en la tierra.",
+			source: "José Mujica",
+			year: "2010"
+		},
+		{
+			quote: "La verdadera riqueza de una nación no está en sus recursos naturales, sino en su gente.",
+			source: "José Batlle y Ordóñez",
+			year: "1904"
+		},
+		{
+			quote: "La educación es la llave maestra para abrir las puertas de la igualdad.",
+			source: "Lucía Topolansky",
+			year: "2018"
+		},
+		{
+			quote: "La verdadera patria del hombre es el hombre mismo.",
+			source: "Jorge Luis Borges",
+			year: "1974"
+		},
+		{
+			quote: "La libertad es una de las más bellas palabras del hombre.",
+			source: "Julio Cortázar",
+			year: "1970"
+		},
+		{
+			quote: "Cada uno tiene su historia dentro de una historia más grande que lo contiene.",
+			source: "Jorge Luis Borges",
+			year: "1960"
+		},
+		{
+			quote: "Era una lengua venida de muy lejos, una lengua que estaba más allá de los mapas, una lengua que no era una lengua.",
+			source: "Alejandra Pizarnik",
+			year: "1968"
+		},
+		{
+			quote: "No es lo mismo tener la llave de una jaula que tener la llave de un mundo.",
+			source: "Isabel Allende",
+			year: "1982"
+		},
+		{
+			quote: "Si tuviera que escribir mi biografía en un par de palabras, diría que fui una lectora empedernida que empezó a escribir por aburrimiento y por necesidad.",
+			source: "María Luisa Bombal",
+			year: "1972"
+		},
+		{
+			quote: "Los ojos son el espejo del alma y reflejan la tristeza que llevamos dentro.",
+			source: "Isabel Allende",
+			year: "1987"
+		},
+		{
+			quote: "Ser poeta es tener en el corazón un pájaro que mueve las alas sin saber por qué y canta sin saber la canción.",
+			source: "Gabriela Mistral",
+			year: "1924"
+		},
+		{
+			quote: "Nunca debemos olvidar que los derechos humanos son derechos sagrados, inherentes a todo ser humano, sin distinción alguna de raza, sexo, nacionalidad, origen étnico, lengua, religión o cualquier otra condición.",
+			source: "Michelle Bachelet",
+			year: "2014"
+		},
+		{
+			quote: "El verdadero amor no es otra cosa que la aceptación total de lo que el otro es.",
+			source: "Clarice Lispector",
+			year: "1977"
+		},
+		{
+			quote: "El amor es una sensación de plenitud, de felicidad, que nos llena el cuerpo, el alma y la mente.",
+			source: "Cora Coralina",
+			year: "1965"
+		},
+		{
+			quote: "No hay nadie que no tenga cicatrices. Pero aquellos que no tienen cicatrices son los que nunca se han arriesgado.",
+			source: "Paula Pimenta",
+			year: "2013"
+		},
+		{
+			quote: "La lucha por la igualdad y la justicia social no es un asunto de uno o dos días, es una lucha permanente y un compromiso de todas y todos los que queremos un mundo mejor.",
+			source: "Gloria Cuartas",
+			year: "2012"
+		},
+		{
+			quote: "La violencia contra las mujeres es la manifestación más extrema de la discriminación y la opresión que sufrimos.",
+			source: "Patricia Ariza",
+			year: "2014"
+		},
+		{
+			quote: "Ser mujer es resistir y transformar la realidad en la que vivimos.",
+			source: "Piedad Bonnett",
+			year: "2018"
+		},
+		{
+			quote: "La educación es el arma más poderosa que tenemos para transformar el mundo.",
+			source: "Marta Traba",
+			year: "1968"
+		},
+		{
+			quote: "El poder es algo que se debe compartir para que todos tengamos voz y voto en las decisiones que nos afectan.",
+			source: "Patricia Lara",
+			year: "2009"
+		},
+		{
+			quote: "La poesía es una forma de resistencia contra el silencio y la opresión.",
+			source: "Gloria Esquivel",
+			year: "2017"
+		},
+		{
+			quote: "El feminismo no es una guerra contra los hombres, es una lucha por la igualdad de derechos y oportunidades.",
+			source: "Alicia Gutiérrez",
+			year: "2015"
+		},
+		{
+			quote: "La cultura es un espacio de encuentro y diálogo entre diferentes, donde podemos reconocernos en nuestra diversidad y construir juntos un futuro mejor.",
+			source: "Consuelo Triviño",
+			year: "2019"
+		},
+		{
+			quote: "La memoria es una forma de resistencia contra el olvido y la impunidad.",
+			source: "Giovanna Rivero",
+			year: "2016"
+		},
+		{
+			quote: "La literatura es una herramienta poderosa para visibilizar las luchas y resistencias de los pueblos y construir una sociedad más justa y equitativa.",
+			source: "María Mercedes Carranza",
+			year: "1990"
+		}
+		
+];
+
+//Function to randomly select a quote value and return a random quote object from the quotes array
+function getRandomQuote () {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	var randomQuote = quotes[randomNumber];
+	return randomQuote;
+}	
+
+//Function to select random rgb color value
+function getRandomColor () {
+	var red = Math.floor(Math.random() * 256 );
+	var green = Math.floor(Math.random() * 256 );
+	var blue = Math.floor(Math.random() * 256 );
+	 var randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	return randomColor;
+}
+
+//Function to call the getRandomQuote function and stores the returned quote object in a variable
+//Constructs a string containing the different properties of the quote object 
+function printQuote () {
+		var quotes = getRandomQuote ();
+		var quoteContainer = document.getElementById("quote-box");
+		var quoteString = `<p class="quote">${quotes.quote}</p><p class="source">${quotes.source}`;
+				if (quotes.citation) {quoteString += `<span class="citation">${quotes.citation}</span>`}
+				if (quotes.year) {quoteString += `<span class="year">${quotes.year}</span></p>`}
+				else {quoteString += '</p>'};
+					quoteContainer.innerHTML = quoteString;
+
+					//assigns random color value to document background color 
+					document.body.style.backgroundColor = getRandomColor ();
+}
+
+//Quote automatically refreshes every 15 seconds
+window.setInterval(function(){
+printQuote ();
+}, 45000);
+
+//Event listener on LoadQuote button to generate new quote		
+document.getElementById("loadQuote").addEventListener("click", printQuote, false);
+
+
+
+
+
